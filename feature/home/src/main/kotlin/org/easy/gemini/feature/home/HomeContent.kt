a48ac948a08fb6cab315f74ea8c9b3fa609a4e67
BreakZero <dejin.po@gmail.com>
2023-12-21 14:28:44 +0800

    finished settings workflow

--- feature/home/src/main/kotlin/org/easy/gemini/feature/home/HomeContent.kt
+++ feature/home/src/main/kotlin/org/easy/gemini/feature/home/HomeContent.kt
@@ -2,6 +2,7 @@ package org.easy.gemini.feature.home
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -23,12 +24,13 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -130,9 +132,16 @@ internal fun HomeScreenContent(
                 }
             }
 
-            else -> {
-
+            is HomeUiState.NeedToSetup -> {
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(text = "Please setup your api key first")
+                }
             }
+
+            else -> Unit
         }
     }
 }
