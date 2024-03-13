58b3cc25ddd656b4b18bd5c8029f828ed9a68b75
BreakZero <dejin.po@gmail.com>
2024-03-13 17:24:43 +0800

    update usage for build logic

--- feature/settings/build.gradle.kts
+++ feature/settings/build.gradle.kts
@@ -1,6 +1,7 @@
 @Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
-    alias(easy.plugins.android.feature.hilt)
+    id("org.easy.android.library.compose")
+    id("org.easy.hilt")
 }
 
 android {
