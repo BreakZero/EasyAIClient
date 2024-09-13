b50130608c06d2f519157c04b38e607590cda3e8
= <dejin.po@gmail.com>
2024-09-13 15:32:19 +0800

    update scripts

--- scripts/new-tag.mk
+++ scripts/new-tag.mk
@@ -1,5 +1,21 @@
-SHELL := /bin/bash
+.PHONY: New Tag
 
-push:
-	git tag $(tag)
-	git push origin $(tag)
+# Variables
+TAG_NAME = $(tag)
+TAG_MESSAGE = "Release version $(tag)"
+
+MAKEFILE_PATH := $(abspath $(lastword $(MAKEFILE_LIST)))
+MAKEFILE_DIR := $(dir $(MAKEFILE_PATH))
+
+new-tag:
+	@echo "Running the shell command..."
+
+	@sh "$(MAKEFILE_DIR)/release_notes.sh"
+
+	@echo "Creating Git tag $(TAG_NAME)..."
+	@git tag -a $(TAG_NAME) -m $(TAG_MESSAGE)
+
+	@echo "Pushing tag $(TAG_NAME) to remote repository..."
+	@git push origin $(TAG_NAME)
+
+	@echo "Release process completed successfully."
